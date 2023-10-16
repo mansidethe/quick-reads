833625a79aae5f608de8776c1fc974d9ca16ecec
mansidethe <mansidethe18@gmail.com>
2023-10-16 08:12:48 +0530

    add news key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
+
 
 function App() {
+  const [news, setNews] = useState([])
+  const loadNews = async () => {
+    try{
+      const response = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2023-09-16&sortBy=publishedAt&apiKey=d17b81c0712d431688c1cdfe245707bc")
+    setNews(response.data.articles)
+    }
+    catch (error) {
+      console.error('Error loading news:', error);
+    }
+    
+  }
+
+  useEffect(() => {
+
+  }, [loadNews])
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Quick Read's</h1>
+      {
+        news.map((newsArticle, index) => {
+          const { author, title, description, urlToImage, publishedArt, content } = newsArticle;
+
+
+         return(
+          <>
+          <h1>{title}</h1>
+          <p>{author}</p>
+          </>
+         )
+        })
+
+      }
+    </div>
   )
 }
 
